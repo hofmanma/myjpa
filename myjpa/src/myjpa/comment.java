e139f1f67e8034b107ba7938b96a5d03278eeb0b
hofmanma <matthhofma@aol.com>
2018-05-15 17:43:56 +0200

    Src

---
+++ myjpa/src/myjpa/comment.java
@@ -0,0 +1,20 @@
+package myjpa;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import javax.persistence.Table;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+@Entity
+@Table(name="comment", catalog = "horizonte", schema = "horizonte") 
+public class comment implements Serializable {
+
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long COMMENTID;
+}
---
+++ myjpa/src/myjpa/song.java
@@ -0,0 +1,23 @@
+package myjpa;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import javax.persistence.Table;
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: song
+ *
+ */
+@Entity
+@Table(name="song", catalog = "horizonte", schema = "horizonte")
+public class song implements Serializable {
+ 
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long SONGID;	
+}
